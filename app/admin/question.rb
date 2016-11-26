e7422e9ff06f9c2810326421c831e9d3b882279f
Selva <c.selvakumar@sedin.co.in>
2016-11-26 21:04:25 +0530

    Added tags

--- app/admin/question.rb
+++ app/admin/question.rb
@@ -1,5 +1,6 @@
 ActiveAdmin.register Question do
-permit_params :title, :description, :sub_category_id, :is_open, :user_id
+permit_params :title, :description, :sub_category_id, :is_open, :user_id, :id,
+              answers_attributes: [ :id, :answer, :is_answered, :_destroy ]
 
 form do |f|
   f.inputs "Add a Question" do
@@ -8,8 +9,28 @@ form do |f|
     f.input :title
     f.input :description
     f.input :is_open
-  end
+    f.inputs do
+      f.has_many :answers, heading: 'Add Answers', allow_destroy: true, new_record: true do |a|
+        a.input :answer
+      end
+    end
+    f.inputs do
+      f.has_many :question_tags, heading: 'Add Tags', allow_destroy: true, new_record: true do |a|
+        a.input :tag
+      end
+    end
+    end
   f.actions
+end
+
+controller do
+  def permitted_params
+    params.permit!
+  end
+  def update
+    super do |format|
+    end
+  end
 end
 
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,9 @@
 class Question < ApplicationRecord
-  # belongs_to :user
+  belongs_to :user
   belongs_to :sub_category
+  has_many :answers
+  has_many :tags
+  has_many :question_tags
+  accepts_nested_attributes_for :answers, :allow_destroy => true
+  accepts_nested_attributes_for :question_tags, :allow_destroy => true
 end
